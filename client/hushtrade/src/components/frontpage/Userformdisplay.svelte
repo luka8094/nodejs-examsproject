31a007a4fad08c674c03215da06d9e2e65b403ca
luka8094 <luka8094@stud.kea.dk>
2022-05-06 14:43:09 +0200

    feat: basic 'sign in' frame component in front-end set upin front-end.

---
+++ client/hushtrade/src/components/frontpage/Userformdisplay.svelte
@@ -0,0 +1,34 @@
+<aside>
+    <h2>Hi!</h2>
+    <p>Welcome to the hushtrade. Please sign in.</p>
+    <form method="post">
+        <label for="useremail">Email</label>
+        <input type="text" id="useremail" name="useremail" placeholder="input your user email">
+        <label for="password">Password</label>
+        <input type="text" id="password" name="password" placeholder="input your password">
+        <input type="submit" value="sign in">
+    </form>
+</aside>
+
+<style>
+    aside{
+        display: flex;
+        flex-direction: column;
+        background: lightgrey;
+        border: dashed 1px black;
+        height: fit-content;
+    }
+
+    form{
+        display: flex;
+        flex-direction: column;
+        align-items: start;
+        justify-content: flex-end;
+        padding: 1em;
+        border-radius: 2px;
+    }
+
+    /* @media and screen(max-width: 414px){
+
+    } */
+</style>
