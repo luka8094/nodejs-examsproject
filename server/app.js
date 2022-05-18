d3ea6948b2defb42fceb492f2b9cd13806efde3a
luka8094 <luka8094@stud.kea.dk>
2022-05-18 13:45:01 +0200

    test: executed 'npm audit' - making a quick scan for potential vulnerabilites.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
+import "dotenv/config"
 import express from "express"
+
 const app = express()
 const PORT = process.env.PORT || 3000
 
 import path from "path"
 app.use(express.static(path.resolve("../client/public")))
+
+import session from "express-session"
+const sessionMiddleware = session({
+    secret: process.env.SECRET_ACCESS_TOKEN,
+    resave: false,
+    saveUninitialized: true
+})
+app.use(sessionMiddleware)
 
 app.listen(PORT, () => console.log("Server running on:", PORT))
